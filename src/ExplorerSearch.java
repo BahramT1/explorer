ce61b6c365f0946878b9a604983a4b96bc5b2e78
Your Name <totakhil.bahram@student.greenriver.edu>
2025-04-07 20:41:56 -0700

    Done with reachableArea method and started working on tests

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -33,8 +33,52 @@ public class ExplorerSearch {
         // Please also make more test cases
         // I STRONGLY RECOMMEND testing some helpers you might make too
 
-        //
         int rows = island.length;
-        return -1;
+        int cols = island[0].length;
+
+
+        int startRow = 0;
+        int startCol = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (island[r][c] == 0) {
+                    startRow = r;
+                    startCol = c;
+                    break;
+                }
+            }
+        }
+
+        boolean[][] visited = new boolean[rows][cols];
+
+        return explore(island, visited, startRow, startCol);
+    }
+
+
+    // A method that explores the map and counts reachable land
+    public static int explore(int[][] island, boolean[][] visited, int r, int c) {
+        // If out of bounds, return 0
+        if (r < 0 || r >= island.length || c < 0 || c >= island[0].length) {
+            return 0;
+        }
+        
+        if (visited[r][c] || island[r][c] == 2 || island[r][c] == 3) {
+            return 0;
+        }
+
+
+        visited[r][c] = true;
+
+
+        int count = 1;
+
+        // Check all 4 directions
+        count += explore(island, visited, r + 1, c);
+        count += explore(island, visited, r - 1, c);
+        count += explore(island, visited, r, c + 1);
+        count += explore(island, visited, r, c - 1);
+
+        return count;
     }
 }
